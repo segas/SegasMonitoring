2da7379ef60bc75fa9d6f2895b02dbb0d7a55552
Stefan Gassner <sgas@segas.ch>
2017-03-06 09:06:17 +0100

    Changed title

---
+++ monitoring_db_site_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sites` (`id_site`, `name`, `description`, `site`, `created`, `last_edited`, `enabled`) VALUES ('1', 'SEGAS', 'SEGAS', 'http://www.segas.ch/index.html', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1');
+INSERT INTO `sites` (`id_site`, `name`, `description`, `site`, `created`, `last_edited`, `enabled`) VALUES ('2', 'Meiertrans', 'Meiertrans Webseite', 'http://www.meiertrans.ch/index.html', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1');
+INSERT INTO `sites` (`id_site`, `name`, `description`, `site`, `created`, `last_edited`, `enabled`) VALUES ('3', 'Google', 'Google Webseite', 'http://www.google.ch/', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1');
+INSERT INTO `sites` (`id_site`, `name`, `description`, `site`, `created`, `last_edited`, `enabled`) VALUES ('4', '20min.ch', '20 Minuten Webseite', 'http://www.20min.ch/', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1');
